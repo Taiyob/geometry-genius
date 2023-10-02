0c41f2d4af39852a438df37a2fff69aee41a8f48
Taiyob <oli1412001@gmail.com>
2023-10-02 10:07:47 +0600

    some geometry calculation

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -1,35 +1,47 @@
 function calculateTriangleArea(){
-    const baseField = document.getElementById('triangle-base');
-    const baseValueText = baseField.value;
-    const base = parseFloat(baseValueText);
-
-    const heightField = document.getElementById('triangle-height');
-    const heightValueText = heightField.value;
-    const height = parseFloat(heightValueText);
+    const base = getInputValue('triangle-base');
+    const height = getInputValue('triangle-height');
     
     const area = 0.5 * base * height;
-
-    const triangleArea = document.getElementById('triangle-area');
-    triangleArea.innerText = area;
-
-    baseField.value = '';
-    heightField.value = '';
+    setInnerText('triangle-area',area);
 }
 
 function calculatorRectangleArea(){
-    const widthField = document.getElementById('rectangle-width');
-    const widthValueText = widthField.value;
-    const width = parseFloat(widthValueText);
-
-    const lengthField = document.getElementById('rectangle-length');
-    const lengthValueText = lengthField.value;
-    const length = parseFloat(lengthValueText);
+    const width = getInputValue('rectangle-width');
+    const length = getInputValue('rectangle-length');
 
     const area = width * length;
-
-    const rectangleArea = document.getElementById('rectangle-area');
-    rectangleArea.innerText = area;
-    
-    widthField.value = '';
-    lengthField.value = '';
+    setInnerText('rectangle-area',area);
 }
+
+function calculateParallelogramArea(){
+    const base = getInputValue('parallelogram-base');
+    const height = getInputValue('parallelogram-height');
+
+    const area = base * height;
+    setInnerText('parallelogram-area',area);
+}
+
+function calculateEllipseArea(){
+    const firstRadius = getInputValue('ellipse-first-radius');
+    const secondRadius = getInputValue('ellipse-second-redius');
+
+    const area = 3.1416 * firstRadius * secondRadius;
+    setInnerText('ellipse-area',area);
+}
+
+// reusable funcction --> reduce duplicate code 
+
+function getInputValue(fieldId){
+    const inputField = document.getElementById(fieldId);
+    const inputValueText = inputField.value;
+    const value = parseFloat(inputValueText);
+    inputField.value = '';
+    return value;
+}
+
+function setInnerText(textId,area){
+    const textElement =  document.getElementById(textId);
+    textElement.innerText = area;
+}
+
